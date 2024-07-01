b097524363d311cfb0d9c6410216d9ee51431337
Kafkasullaspiaggia <hanz.butor@gmail.com>
2024-07-01 10:30:12 +0100

    Update script.js

--- banner/script.js
+++ banner/script.js
@@ -1,6 +1,6 @@
 
 function redirectToXcom() {
-    window.open("https://announce.connect.corp.google.com/agent/announcements/20863315?a=1685035473");
+    window.open("https://announce.connect.corp.google.com/agent/announcements/62715642?a=1685035473");
 }
 
 const bannerContainer = document.querySelector('.banner-container');
