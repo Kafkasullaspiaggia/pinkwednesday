767a1631a0ac826ace130c6a44b33c54adb88f2b
Kafkasullaspiaggia <hanz.butor@gmail.com>
2024-07-01 12:10:51 +0100

    update

---
+++ bannernest/script.js
@@ -0,0 +1,7 @@
+
+function redirectToXcom() {
+    window.open("https://announce.connect.corp.google.com/agent/announcements/99851933?a=1685035473");
+}
+
+const bannerContainer = document.querySelector('.banner-container');
+bannerContainer.addEventListener('click', redirectToXcom);
